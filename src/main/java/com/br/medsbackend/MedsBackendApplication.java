1c844630bc5402d0c252a5d3f35ad32d5513b0fc
edugabriel12 <gabriellima4539@yahoo.com>
2024-07-18 20:19:04 -0300

    Primeiro push

---
+++ src/main/java/com/br/medsbackend/MedsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.medsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedsBackendApplication.class, args);
+    }
+
+}
